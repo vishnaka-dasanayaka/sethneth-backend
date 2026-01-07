78a1f80e44edf4df376e4505a6b3d3416bd2e18d
Vishnaka Dasanayaka <vishnakadasanayaka@gmail.com>
2026-01-08 01:16:11 +0530

    change-password, add note for inv

---
+++ api/models/Note.js
@@ -0,0 +1,36 @@
+/**
+ * Note.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  tableName: "notes",
+
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    type: { type: "string", required: true },
+    f_key: { type: "number", required: true },
+    note: { type: "string", required: true },
+    status: { type: "number", defaultsTo: 2 },
+    created_on: { type: "ref", columnType: "datetime", autoCreatedAt: true },
+    created_by: { model: "User" },
+    updated_on: { type: "ref", columnType: "datetime" },
+    updated_by: { model: "User" },
+
+    // 2 - active
+    // -2 - deleted
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+};
